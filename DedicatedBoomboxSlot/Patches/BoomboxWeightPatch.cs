ef476428aacdb31bbb53d3543f3edb6f3abff9b6
GerekeWill <gerekewill@gmail.com>
2024-12-08 12:35:58 -0500

    Add files via upload

---
+++ DedicatedBoomboxSlot/Patches/BoomboxWeightPatch.cs
@@ -0,0 +1,34 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DedicatedBoomboxSlot.Patches
+{
+    [HarmonyPatch(typeof(GrabbableObject))]
+    public class BoomboxWeightPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void ChangeItemWeight(GrabbableObject __instance)
+        {
+            Plugin.Log("Attempting to edit item...");
+            if (__instance == null)
+            {
+                return;
+            }
+            if (__instance.itemProperties.itemName == "Boombox")
+            {
+                __instance.itemProperties.weight = 0f;
+                Plugin.Log("Weight Updated Successfully");
+            }
+            else
+            {
+                Plugin.Log("Item was not Boombox. Continuing. . .");
+            }
+        }
+    }
+}
---
+++ DedicatedBoomboxSlot/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DedicatedBoomboxSlot
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "KurisuBot.DedicatedBoomboxSlot";
+        private const string modName = "DedicatedBoomboxSlot";
+        private const string modVersion = "1.0.0";
+        public static Plugin instance;
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("DedicatedBoomboxSlot Loading . . . ");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+
+
+            mls.LogInfo("DedicatedBoomboxSlot Loaded!");
+        }
+
+        public static void Log(string message)
+        {
+             Plugin.instance.Logger.LogInfo(message);
+        }
+    }
+}
